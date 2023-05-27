b6cd5ddf2ee58678a0168d2d45cb0c22950e1cc8
oguchi nzekwe <n.zekwe@gmail.com>
2023-05-27 18:59:50 +0100

    structure updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./assets/css/style.css/");
-    eleventyConfig.addWatchTarget("./assets/css/style.css/");
+    eleventyConfig.addPassthroughCopy("./public/css/style.css/");
+    eleventyConfig.addPassthroughCopy("./public/img/");
+    eleventyConfig.addWatchTarget("./public/css/style.css/");
+
+    return {
+      dir: {
+        //input: "src",
+        includes: "_includes",
+        data: "_data",
+        output: "_site"
+      }
+    }
   };
